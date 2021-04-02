4bcb9075f38ff59aa8dd0ea9f4bcf719fa0125ce
Rodrigo Mendes <rcmendes80@gmail.com>
2021-04-02 10:38:22 -0400

    added cli entry folder.

---
+++ cmd/cli/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/rcmendes/crud-example-go/internal/services/core/usecases"
+	"github.com/rcmendes/crud-example-go/internal/services/storage/database"
+)
+
+//TODO Create log level and init function
+
+func main() {
+	database.InitDB()
+	database.CreateTables()
+
+	servicesStorage := database.SQLite3ServicesStorage{}
+	serviceManager := usecases.NewServiceManager(&servicesStorage)
+
+	for i := 1; i <= 10; i++ {
+		name := fmt.Sprintf("Name %d", i)
+		description := fmt.Sprintf("Description %d", i)
+		command := usecases.CreateServiceCommand{Name: name, Description: &description}
+
+		if err := serviceManager.Create(command); err != nil {
+			log.Printf("%+q\n", err)
+		}
+	}
+
+	list, err := serviceManager.ListAllServices()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("List of Services:")
+	for i, s := range list {
+		fmt.Printf("%d: %s\n", i, s.String())
+	}
+
+	for i := 1; i <= 2; i++ {
+		name := fmt.Sprintf("Name %d", i)
+		description := fmt.Sprintf("Description %d", i)
+		command := usecases.CreateServiceCommand{Name: name, Description: &description}
+
+		if err := serviceManager.Create(command); err != nil {
+			log.Printf("%+q\n", err)
+		}
+	}
+}
